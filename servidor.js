2392f3cf0e980059f96b53abec765dce0c182864
decesarojunior <decesarojunior@yahoo.com.br>
2024-08-20 11:12:09 -0300

    servico de list e insert patente

---
+++ servidor.js
@@ -0,0 +1,167 @@
+var express = require('express'); // requisita a biblioteca para a criacao dos serviços web.
+var pg = require("pg"); // requisita a biblioteca pg para a comunicacao com o banco de dados.
+
+ var sw = express(); // iniciliaza uma variavel chamada app que possitilitará a criação dos serviços e rotas.
+
+sw.use(express.json());//padrao de mensagens em json.
+//permitir o recebimento de qualquer origem, aceitar informações no cabeçalho e permitir o métodos get e post
+sw.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET,POST');
+    next();
+});
+
+
+const config = {
+    host: 'localhost',
+    user: 'postgres',
+    database: 'db_cs_2024',
+    password: '123456',
+    port: 5432
+};
+
+//definia conexao com o banco de dados.
+const postgres = new pg.Pool(config);
+
+//definicao do primeiro serviço web.
+sw.get('/', (req, res) => {
+    res.send('Hello, world! meu primeiro teste.  #####');
+})
+
+sw.get('/teste', (req, res) => {
+    res.status(201).send('meu teste');
+})
+
+sw.get('/listenderecos', function (req, res, next) {
+    
+    postgres.connect(function(err,client,done) {
+
+       if(err){
+
+           console.log("Nao conseguiu acessar o  BD "+ err);
+           res.status(400).send('{'+err+'}');
+       }else{            
+
+            var q ='select codigo, complemento, cep, nicknamejogador ' +
+                    'from tb_endereco order by codigo asc';            
+    
+            client.query(q,function(err,result) {
+                done(); // closing the connection;
+                if(err){
+                    console.log('retornou 400 no listendereco');
+                    console.log(err);
+                    
+                    res.status(400).send('{'+err+'}');
+                }else{
+
+                    //console.log('retornou 201 no /listendereco');
+                    res.status(201).send(result.rows);
+                }           
+            });
+       }       
+    });
+});
+
+sw.get('/listpatentes', function (req, res, next) {
+    
+    postgres.connect(function(err,client,done) {
+
+       if(err){
+           console.log("Nao conseguiu acessar o  BD "+ err);
+           res.status(400).send('{'+err+'}');
+       }else{            
+
+            var q ='select codigo, nome, quant_min_pontos, cor, logotipo, to_char(datacriacao, \'dd/mm/yyyy hh24:mi:ss\') as datacriacao  from tb_patente;';            
+    
+            client.query(q,function(err,result) {
+                done(); // closing the connection;
+                if(err){
+                    console.log('retornou 400 no listendereco');
+                    console.log(err);
+                    
+                    res.status(400).send('{'+err+'}');
+                }else{
+
+                    //console.log('retornou 201 no /listendereco');
+                    res.status(201).send(result.rows);
+                }           
+            });
+       }       
+    });
+});
+
+
+sw.get('/listjogadores', function (req, res, next) {    
+    postgres.connect(function(err,client,done) {
+       if(err){
+           console.log("Nao conseguiu acessar o  BD "+ err);
+           res.status(400).send('{'+err+'}');
+       }else{            
+            var q ='select j.nickname, j.senha, 0 as patentes, e.cep '+
+                        'from tb_jogador j, tb_endereco e '+
+                        'where e.nicknamejogador=j.nickname order by nickname asc;';    
+            //Exercicio 1: incluir todas as colunas de tb_endereco
+            client.query(q,async function(err,result) {
+                
+                if(err){
+                    console.log('retornou 400 no listjogadores');
+                    console.log(err);                    
+                    res.status(400).send('{'+err+'}');
+                }else{
+                    for(var i=0; i < result.rows.length; i++){                                              
+                        try { //Exercicio 2: incluir todas as colunas de tb_patente.                         
+                              pj = await client.query('select codpatente from '+
+                                                      'tb_jogador_conquista_patente '+
+                                                      'where nickname = $1', 
+                                                             [result.rows[i].nickname])                                                    
+                              result.rows[i].patentes = pj.rows;    
+                        } catch (err) {                                                       
+                            res.status(400).send('{'+err+'}');
+                        }                                           
+                    }
+                    done(); // closing the connection;
+                    //console.log('retornou 201 no /listendereco');
+                    res.status(201).send(result.rows);
+                }           
+            });
+       }       
+    });
+});
+
+sw.post('/insertpatente', function(req, res, next){
+
+    postgres.connect(function(err,client,done) {
+        if(err){
+            console.log("Nao conseguiu acessar o  BD "+ err);
+            res.status(400).send('{'+err+'}');
+        }else{
+
+            var q1 = {
+                text: 'insert into tb_patente (nome, quant_min_pontos, datacriacao, cor, logotipo) '
+                       + ' values ($1, $2, now(), $3, $4) returning codigo, nome, quant_min_pontos, '+
+                       'to_char(datacriacao, \'dd/mm/yyyy hh24:mi:ss\') as datacriacao, logotipo ',
+                values : [req.body.nome, req.body.quant_min_pontos, req.body.cor, req.body.logotipo]
+            }
+            client.query(q1, function(err,result1) {
+                if(err){
+                    console.log('retornou 400 no insert q1');
+                    res.status(400).send('{'+err+'}');
+                }else{
+                    console.log('retornou 201 no insertpatente');
+                    res.status(201).send({"codigo": result1.rows[0].codigo,
+                                          "nome"  : result1.rows[0].nome,
+                                          "quant_min_pontos" : result1.rows[0].quant_min_pontos,
+                                          "datacriacao" : result1.rows[0].datacriacao,
+                                          "logotipo" : result1.rows[0].logotipo
+                    });
+                }
+            });
+
+        }
+    });
+});
+
+sw.listen(4000, function () {
+    console.log('Server is running.. on Port 4000');
+});
